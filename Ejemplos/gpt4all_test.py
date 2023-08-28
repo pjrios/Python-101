d0acfedd4722e04fd4f064b8a8e7dfc3a81e5366
pjrios <98301213+pjrios@users.noreply.github.com>
2023-08-27 20:19:40 -0500

    moving files

--- Ejemplos/gpt4all_test.py
+++
@@ -1,8 +0,0 @@
-# Librerías
-from gpt4all import GPT4All
-
-# Importar modelo
-model = GPT4All("C:\\Users\\pjrio\\AppData\\Local\\nomic.ai\\GPT4All\\nous-hermes-13b.ggmlv3.q4_0.bin")
-
-output = model.generate("Me podrías ayudar a hacer un programa de Python para contar de 10 en 10 hasta 1500?")
-print(output)
